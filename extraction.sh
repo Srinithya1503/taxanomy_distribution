1abeccaa8c0d9c10ea36c256744e481f449081f9
Srinithya1503 <venkatsri1503@gmail.com>
2024-08-12 18:15:53 +0530

    Add files via upload
    if you have downloaded the genome information of a bulk number of species, let's say of a phylum from NCBI genome browser and now you want to see the taxonomy distribution of each of them. you can achieve this by following these:
    1) download the rankedlineage.dmp file from NCBI taxonomy browser.
    2)using shell script i have uploaded  "extraction.sh", the required taxonomy distribution would be achieved as in the output file "taxa_696_dataset.tsv".

---
+++ extraction.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Set the input filenames
+genome_dataset="696_genome_dataset.tsv"
+rankedlineage="rankedlineage.dmp"
+output_tsv="taxa_696_dataset.tsv"
+
+# Header line for the output TSV
+echo -e "taxonomy_id\tspecies\tgenus\tfamily\torder\tclass\tphylum\tkingdom" > "$output_tsv"
+
+# Process each organism name in the genome dataset
+awk -F'\t' 'NR>1 { print $3 }' "$genome_dataset" | while read -r organism_name; do
+
+    # Trim leading and trailing whitespace from the organism name
+    organism_name=$(echo "$organism_name" | sed 's/^ *//;s/ *$//')
+
+    # Use grep to find the organism name in the rankedlineage.dmp file
+    grep -F "$organism_name" "$rankedlineage" | awk -F'\t\\|\t' -v org="$organism_name" '
+    BEGIN {
+        OFS = "\t"  # Set the output field separator to a tab
+    }
+    {
+        # Remove leading and trailing spaces from all fields and remove trailing "|"
+        for (i = 1; i <= NF; i++) {
+            gsub(/^ +| +$|\\|$/, "", $i)
+        }
+        # Check if the second field matches the organism name
+        if ($2 == org) {
+            # Print the desired columns in the output format
+            print $1, $2, $4, $5, $6, $7, $8, $10
+        }
+    }' >> "$output_tsv"
+done
+
+echo "TSV file '$output_tsv' created."
+
